51883b57237afd4aa3de3e7f45d1d30fb5b6480c
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-03-31 20:32:15 +0100

    removed api to be on its own as a separate repository

--- api/routes/game.js
+++
@@ -1,9 +0,0 @@
-//Requires
-const express = require('express');
-const router = express.Router();
-const gameController = require('../controllers/game');
-
-router.get('/levels', gameController.getLevels);
-router.get('/levels/:id', gameController.getLevelById);
-
-module.exports = router;
--- api/routes/profilePic.js
+++
@@ -1,9 +0,0 @@
-//Requires
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/users')
-
-router.get('/', usersController.getProfilePics);
-router.get('/:id', usersController.getProfilePicsById);
-
-module.exports = router;
